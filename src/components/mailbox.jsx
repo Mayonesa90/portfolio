8ca66ec60d298af10b246777978168b34ee84528
Maya Arzapalo Björklund <mayonaas@hotmail.com>
2025-01-14 22:22:18 +0100

    added send mail funciton to mailbow

--- src/components/mailbox.jsx
+++ src/components/mailbox.jsx
@@ -13,9 +13,21 @@ export function Mailbox(){
         return () => clearInterval(interval);
     }, []);
 
+    // Handle mailbox click event to open default email client
+    const handleMailboxClick = () => {
+        // Trigger a mailto link
+        window.location.href = 'mailto:mayarzapalo@icloud.com?subject=Mail from Mailbox&body=Hello!';
+    };
+
+
     return (
         <div className='fixed bottom-3 right-3 z-30'>
-            <img className='w-12 md-custom:w-16 lg-custom:w-20' src={mailbox ? MailboxFlagDown : MailboxFlagUp} alt="Mailbox click to send mail" />
+            <img 
+                className='w-12 md-custom:w-16 lg-custom:w-20' 
+                src={mailbox ? MailboxFlagDown : MailboxFlagUp} 
+                alt="Mailbox click to send mail" 
+                onClick={handleMailboxClick}
+            />
         </div>
     );
 }
